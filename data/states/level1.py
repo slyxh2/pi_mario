173f68066efd580d049da94cb2c3472afac7ac58
yichunhuang0805 <yichunhuang@brandeis.edu>
2024-12-02 21:22:32 -0600

    debugging

--- data/states/level1.py
+++ data/states/level1.py
@@ -1324,9 +1324,9 @@ class Level1(tools._State):
             self.mario.x_vel = 0
             self.state = c.FROZEN
             self.game_info[c.MARIO_DEAD] = True
-            self.led_controller.lit_thread(255, 0, 0, 1)
 
         if self.mario.dead:
+            self.led_controller.lit_thread(255, 0, 0, 1)
             self.play_death_song()
 
 
