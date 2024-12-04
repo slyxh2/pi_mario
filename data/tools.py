d49e9bc6f6473eb79cf292844bf880c0aeed0f9c
yichunhuang0805 <yichunhuang@brandeis.edu>
2024-12-03 22:27:28 -0600

    Add clear after welcome LCD

--- data/tools.py
+++ data/tools.py
@@ -116,7 +116,7 @@ class Control(object):
             self.led_controller.pattern_left_to_right_thread(255, 255, 255, delay=0.5)
             self.led_controller.clear()
         
-        self.lcd_controller.display_message("Welcome!")
+        self.lcd_controller.display_message("Welcome!", "", 3)
         
         """Main loop for entire program"""
         while not self.done:
