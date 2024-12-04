0ee87db7b3581e68089f437b5016aa745ea7aaae
yichunhuang0805 <yichunhuang@brandeis.edu>
2024-12-03 23:31:17 -0600

    test

--- data/states/level1.py
+++ data/states/level1.py
@@ -367,6 +367,7 @@ class Level1(tools._State):
         self.sound_manager.update(self.game_info, self.mario)
         if self.overhead_info_display.state != c.FAST_COUNT_DOWN or self.overhead_info_display.time <= 0:
             if not self.welcomed:
+                self.welcomed = True
                 self.handle_score_update(self.game_info[c.SCORE], self.game_info[c.LIVES])
         else:
             self.lcd_controller.keep_display_message("Calculating...")
